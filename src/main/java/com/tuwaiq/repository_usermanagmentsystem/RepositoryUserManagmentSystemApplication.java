9b47ee42f5eb3a95bc75d132d1dcd6eabf5fbf9d
osama dad <osamadad768@gmail.com>
2025-11-25 14:12:02 +0300

    initial commit

---
+++ src/main/java/com/tuwaiq/repository_usermanagmentsystem/RepositoryUserManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.repository_usermanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryUserManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryUserManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuwaiq/repository_usermanagmentsystem/RepositoryUserManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.repository_usermanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryUserManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
